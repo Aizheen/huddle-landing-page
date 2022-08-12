9b908acf6fa29f29e07ac52ae3b61cd42a51a2dc
Aizheen <lautarologgia@gmail.com>
2022-08-12 13:57:51 -0300

    Huddle_1.0

---
+++ index/main.js
@@ -0,0 +1,56 @@
+
+window.sr = ScrollReveal();
+
+sr.reveal(".hero__title",{
+    duration:3000,
+    origin: "bottom",
+    distance:"-50px"
+})
+
+sr.reveal(".hero__paragraph",{
+    duration:3500,
+    origin:"top",
+    distance:"-100px"
+})
+
+sr.reveal(".cta",{
+    duration:3500,
+    origin:"top",
+    distance:"-100px"
+})
+sr.reveal(".hero__picture",{
+    duration:3500,
+    origin:"top",
+    distance: "-50px"
+
+})
+
+
+
+sr.reveal(".hero__numbers",{
+    duration:3500,
+    origin:"top",
+    distance:"-100px"
+})
+
+
+
+sr.reveal(".subtitle",{
+    duration:3000,
+    origin: "top",
+    distance:"100px"
+})
+
+sr.reveal(".section__paragraph",{
+    duration:3000,
+    origin: "bottom",
+    distance:"100px"
+})
+
+
+
+sr.reveal(".section__img",{
+    duration:3000,
+    origin: "left",
+    distance:"100px"
+})
